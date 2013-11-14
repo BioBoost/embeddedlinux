2bcfd2b01a115484557d69cb81194cfee0942a18
Nico De Witte <dewittenico@gmail.com>
2013-11-14 13:22:12 +0100

    Added time elapsed since load of module and did some small cleanup actions

--- beaglebone/modules/hello_module/hello.c
+++ beaglebone/modules/hello_module/hello.c
@@ -6,16 +6,24 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/time.h>
+
+/* Used for tracking time the module ran */
+struct timeval start_tv;
+struct timeval end_tv;
 
 static int __init hello_init(void)
 {
-	pr_alert("Good morning mister kernel man.\n");
+	pr_info("hello: Good morning mister kernel man.");
+	do_gettimeofday(&start_tv);
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	pr_alert("Outta here\n");
+	do_gettimeofday(&end_tv);
+	pr_info("hello: Outta here. Module ran for %lld seconds",
+		((long long)end_tv.tv_sec - (long long)start_tv.tv_sec));
 }
 
 module_init(hello_init);
@@ -23,5 +31,3 @@ module_exit(hello_exit);
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Greets the kernel hello");
 MODULE_AUTHOR("We all are.");
-
-
