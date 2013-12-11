81f81576f47d1314e985b011db95325dbcc36a42
Nico De Witte <nico.dewitte@khbo.be>
2013-12-11 11:10:09 +0100

    Added basic i2c slave functionality to the mbed led cube firmware

--- beaglebone/modules/i2c_mbed_ledcube_module/mbed_i2c_slave_ledcube.c
+++ beaglebone/modules/i2c_mbed_ledcube_module/mbed_i2c_slave_ledcube.c
@@ -4,17 +4,30 @@ DigitalOut din(p15);
 DigitalOut clk(p16);
 DigitalOut le(p17);
 
+I2CSlave slave(p9, p10);    // An I2C Slave, used for communicating with an I2C Master device
+DigitalOut led_read(LED1);
+DigitalOut led_write(LED2);
+
 enum CUBE_CONSTANTS {
     CUBE_LEVELS = 5,
     DATA_SIZE = 32,
-    CUBE_LEVEL_DELAY = 4     // ms (4 seems to be minimum)
+    DATA_BYTES = 20,        // Number of bytes for a full cube pattern
+    CUBE_LEVEL_DELAY = 4    // ms (4 seems to be minimum)
+};
+
+enum I2C_CONSTANTS {
+    I2C_SLAVE_ADDRESS = 0xD0,       // 0x68 on linux
+    I2C_FREQUENCY = 100000,
+    I2C_BUFFER_SIZE = 21            // Command + 4bytes x 5 levels
+};
+
+enum I2C_COMMANDS {
+    WRITE_CUBE = 0x00
 };
 
 void outputData(int data)
 {
-    int i;
-
-    for (i = 0; i < DATA_SIZE; i++) {
+    for (int i = 0; i < DATA_SIZE; i++) {
         clk = 0;
         din = ((data >> i) & 0x0000001);
         clk = 1;
@@ -36,15 +49,62 @@ int main()
     };
     unsigned char ilevel = 0;
 
+    // I2C
+    char buffer[I2C_BUFFER_SIZE];
+    int i2cstatus;
+
+    // Disable cube
     clk = 0;
     din = 0;
     le = 0;
+    outputData(0xF8000000);     // Clear cube
 
+    // Configure slave
+    slave.frequency(I2C_FREQUENCY);
+    slave.address(I2C_SLAVE_ADDRESS);
+
+    // Eternity
     while(1)
     {
         outputData(cubedata[ilevel]);
-        
-        wait_ms(CUBE_LEVEL_DELAY);        
-        ilevel = (ilevel + 1) % 5;
+
+        i2cstatus = slave.receive();
+        switch (i2cstatus)
+        {
+            case I2CSlave::WriteAddressed:      // The master is writing to this slave
+                if (slave.read(buffer, I2C_BUFFER_SIZE))
+                {
+                    led_write = !led_write;
+
+                    switch (buffer[0])      // Command byte
+                    {
+                        case WRITE_CUBE:    // Write cube pattern
+                            memcpy(cubedata, (buffer+1), DATA_BYTES);
+                            printf("Wrote cube pattern\r\n");
+                            break;
+
+                        default:
+                            printf("Unknown command byte\r\n");
+                            break;
+                    }
+                }
+                else
+                {
+                    printf("Could not retrieve i2c data\r\n");
+                }
+                break;
+
+            case I2CSlave::ReadAddressed:       // the master has requested a read from this slave
+                led_read = !led_read;
+                printf("Read request received\r\n");
+                break;
+        }
+
+        for (int i = 0; i < I2C_BUFFER_SIZE; i++) {
+            buffer[i] = 0;
+        }
+
+        wait_ms(CUBE_LEVEL_DELAY);
+        ilevel = (ilevel + 1) % CUBE_LEVELS;
     }
 }
