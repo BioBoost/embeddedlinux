e2e558b65050761230db484809777515b751dfef
Nico De Witte <nico.dewitte@khbo.be>
2013-12-11 17:15:14 +0100

    Added ping pong buffering system to cube

--- beaglebone/modules/i2c_mbed_ledcube_module/mbed_i2c_slave_ledcube.c
+++ beaglebone/modules/i2c_mbed_ledcube_module/mbed_i2c_slave_ledcube.c
@@ -25,7 +25,7 @@ enum I2C_COMMANDS {
     WRITE_CUBE = 0x00
 };
 
-void outputData(int data)
+void outputData(unsigned int data)
 {
     for (int i = 0; i < DATA_SIZE; i++) {
         clk = 0;
@@ -40,14 +40,25 @@ void outputData(int data)
 
 int main()
 {
-    int cubedata[CUBE_LEVELS] = {   // 32 bits per level and 5 levels
-        0xF0000001,                 // Some hardcoded test data
+    // Ping pong buffering
+    unsigned int cubebuffer_0[CUBE_LEVELS] = {   // 32 bits per level and 5 levels
+        0xF0000001,                     // Some hardcoded test pattern
         0xE8000002,
         0xD8000004,
         0xB8000008,
         0x78000010
     };
+    unsigned int cubebuffer_1[CUBE_LEVELS] = {   // 32 bits per level and 5 levels
+        0xF0000001,                     // Some hardcoded test pattern
+        0xE8000002,
+        0xD8000004,
+        0xB8000008,
+        0x78000010
+    };
+    unsigned int * currentbuffer = cubebuffer_0;
+    unsigned int * otherbuffer = cubebuffer_1;
     unsigned char ilevel = 0;
+    bool switchbuffers = false;
 
     // I2C
     char buffer[I2C_BUFFER_SIZE];
@@ -58,6 +69,7 @@ int main()
     din = 0;
     le = 0;
     outputData(0xF8000000);     // Clear cube
+    printf("Mbed cube started and cleared. Awaiting commands\r\n");
 
     // Configure slave
     slave.frequency(I2C_FREQUENCY);
@@ -66,21 +78,21 @@ int main()
     // Eternity
     while(1)
     {
-        outputData(cubedata[ilevel]);
+        outputData(currentbuffer[ilevel]);
 
         i2cstatus = slave.receive();
         switch (i2cstatus)
         {
             case I2CSlave::WriteAddressed:      // The master is writing to this slave
-                if (slave.read(buffer, I2C_BUFFER_SIZE))
+                if (slave.read(buffer, I2C_BUFFER_SIZE) == 0)
                 {
                     led_write = !led_write;
 
                     switch (buffer[0])      // Command byte
                     {
                         case WRITE_CUBE:    // Write cube pattern
-                            memcpy(cubedata, (buffer+1), DATA_BYTES);
-                            printf("Wrote cube pattern\r\n");
+                            memcpy((char *)otherbuffer, (buffer+1), DATA_BYTES);
+                            switchbuffers = true;
                             break;
 
                         default:
@@ -88,23 +100,38 @@ int main()
                             break;
                     }
                 }
-                else
-                {
+                else {
                     printf("Could not retrieve i2c data\r\n");
                 }
                 break;
 
             case I2CSlave::ReadAddressed:       // the master has requested a read from this slave
-                led_read = !led_read;
-                printf("Read request received\r\n");
+                if (slave.read(buffer, I2C_BUFFER_SIZE) == 0) {
+                    led_read = !led_read;
+                    printf("Read request received.\r\n");
+                }
+                else {
+                    printf("Read request received. FAIL.\r\n");
+                }
+                break;
+
+            default:
                 break;
         }
 
+        // Clear I2C buffer
         for (int i = 0; i < I2C_BUFFER_SIZE; i++) {
             buffer[i] = 0;
         }
 
-        wait_ms(CUBE_LEVEL_DELAY);
         ilevel = (ilevel + 1) % CUBE_LEVELS;
+
+        // Switch buffers
+        if (ilevel == 0 && switchbuffers) {
+            unsigned int * tmp = currentbuffer;
+            currentbuffer = otherbuffer;
+            otherbuffer = tmp;
+            switchbuffers = false;
+        }
     }
 }
